2b6fe3da0c1ae8c020aa043f784eaff3eea7f578
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-05-27 23:48:49 +0400

    It seems like the bug with deleting root was fixed, I rewrote root with null, if the remove key was in the root node, now it replaces root with one of its children and then add another children

--- src/main/java/gb/classwork/lesson4/binarysearchthree/BinarySearchTree.kt
+++ src/main/java/gb/classwork/lesson4/binarysearchthree/BinarySearchTree.kt
@@ -5,7 +5,10 @@ import gb.classwork.lesson4.TreeNode
 import gb.classwork.lesson4.binarysearchthree.exceptions.KeyAlreadyAddedException
 import gb.classwork.lesson4.binarysearchthree.exceptions.KeyNotFoundException
 import gb.classwork.lesson4.binarysearchthree.exceptions.UnexpectedException
-
+/**
+ * Данный класс предоставляет реализацию двоичного дерева поиска без балансировки
+ * @author David Blbulyan
+ * */
 class BinarySearchTree<K: Comparable<K>, V>: AbstractStorage<K, V> {
     private var size: Int = 0
     private var root: TreeNode<K, V>? = null;
@@ -19,23 +22,38 @@ class BinarySearchTree<K: Comparable<K>, V>: AbstractStorage<K, V> {
 
     override fun remove(key: K): V {
         // FIXME: Исправить эту функцию, при удалении почему-то удаляется то, что не должно было удалится
-        val nodeForDelete = findNode(key);
+        // FIXME: Здесь ещё внезапно может вылететь исключение KeyAlreadyAddedException проверить почему !!!
+        val nodeForDelete = findNode(key);//ищем удаляемую ноду
         val value = nodeForDelete.value
         if(nodeForDelete.hasParent()){//если наш найденный элемент это не корень
             val parent: TreeNode<K, V> = nodeForDelete.parent ?: throw UnexpectedException("WTF, сюда не мог попасть null!")
             if(parent.left === nodeForDelete){
+                //у нас удаляемый элемент левый в parent, значит он меньше чем parent, а значит правый элемент в удаляемом элементе тоже будет меньше чем parent
                 parent.left = nodeForDelete.left
+                parent.left?.parent = parent//установка нового родителя
                 val right = nodeForDelete.right
-                if(right != null)findPlaceAndInsertNode(right)
+                if(right != null)findPlaceAndInsertNode(right)//если правый элемент у удаляемого был, добавляем его
             }
             else if(parent.right === nodeForDelete){
+                //у нас удаляемый элемент правый в parent, значит он больше parent, значит правый элемент удаляемого элемента тоже будет больше parent
                 parent.right = nodeForDelete.right
+                parent.right?.parent = parent//установка нового родителя
                 val left = nodeForDelete.left
-                if(left != null)findPlaceAndInsertNode(left)
+                if(left != null)findPlaceAndInsertNode(left)//если левый элемент у удаляемого был, добавляем его
             }
             else throw UnexpectedException("Что-то пошло не так и родительский элемент не является родительским элементом...")
         }
-        else root = null
+        else {
+            //здесь вероятно нужно более детально выбирать какой ребёнок теперь станет корневым
+            val newRoot: TreeNode<K, V>? = (root?.right ?: root?.left) //выбираем правого если он не null, иначе выбираем левого
+            val futureChild: TreeNode<K, V>? = (root?.left ?: root?.right)//а здесь наоборот, выбираем левого если он не null, иначе правого
+            newRoot?.parent = null;
+            root = newRoot//если новый корень null, значит у него не было потомков
+            if(newRoot != null){//бум, мы точно знаем что у старого корня были дети
+                if(newRoot !== futureChild && futureChild != null)
+                    findPlaceAndInsertNode(futureChild)
+            }
+        }
         size--
         return value
     }
