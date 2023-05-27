4ed436f679f193df91ed26498a55bb3003994a51
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-05-27 10:27:03 +0400

    Linked lists added

---
+++ src/main/java/gb/classwork/lesson3/OneLinkedList.java
@@ -0,0 +1,51 @@
+package gb.classwork.lesson3;
+
+public class OneLinkedList implements AbstractList{
+    private static class Node{
+        Node next;
+        int value;
+    }
+    Node head;
+    public void addBegin(int value){
+        Node newNode = new Node();
+        newNode.value = value;
+        if(head != null) newNode.next = head;
+        head = newNode;
+    }
+    public void removeBegin(){
+        if(head != null){
+            head = head.next;
+        }
+    }
+    public boolean find(int value){
+        Node currentNode = head;
+        while (currentNode != null){
+            if(currentNode.value == value)return true;
+            currentNode = currentNode.next;
+        }
+        return false;
+    }
+    public void addEnd(int value){
+        Node newNode = new Node();
+        newNode.value = value;
+        if(head == null)head = newNode;
+        else{
+            Node lastNode = head;
+            while (lastNode.next != null)lastNode = lastNode.next;
+            lastNode.next = newNode;
+        }
+    }
+    public void removeEnd(){
+        if(head != null){
+            Node preLastElement = head;
+            while (preLastElement.next != null){
+                if(preLastElement.next.next == null){
+                    preLastElement.next = null;
+                    return;
+                }
+                preLastElement = preLastElement.next;
+            }
+            head = null;
+        }
+    }
+}
---
+++ src/main/java/gb/classwork/lesson3/TwoLinkedList.java
@@ -0,0 +1,59 @@
+package gb.classwork.lesson3;
+
+public class TwoLinkedList {
+    private static class Node{
+        Node next;
+        Node previous;
+        int value;
+        Node(int value){
+            this.value = value;
+        }
+        Node(){}
+    }
+    private Node head;
+    private Node tail;
+    /**
+     * Функция добавляет элемент в начало списка
+     * */
+    public void add(int value){
+        Node node = new Node(value);
+        if(head == null){
+            head = tail = node;
+        }
+        else{
+            head.previous = node;
+            node.next = head;
+            head = node;
+        }
+    }
+    public void addEnd(int value){
+        Node node = new Node(value);
+        if(tail == null){
+            head = tail = node;
+        }
+        else {
+            tail.next = node;
+            node.previous = tail;
+            tail = node;
+        }
+    }
+    public void removeEnd(){
+        if(tail == null)return;
+        if(tail.previous == null){
+            head = tail = null;
+        }
+        else{
+            tail = tail.previous;
+            tail.next = null;
+        }
+    }
+    public void removeBegin(){
+        if(head == null)return;
+        if(head.next == null)head = null;
+        else {
+            head = head.next;
+            head.previous = null;
+        }
+    }
+
+}
